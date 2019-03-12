434450d89107a88226eb637eff1f1d10fd987a12
Sapar Atayev <sopa.97@mail.ru>
2019-03-12 21:46:35 +0300

    feat: add function, which controls brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // if(str.length % 2){
+    // for (var i=0; i<bracketsConfig.length; i++) {
+    //   bracketsConfig[i] = bracketsConfig[i].join('');
+    // }
+
+    var arr2=[];
+
+    for(i=0;i<bracketsConfig.length;i++) {
+      arr2.push(bracketsConfig[i]);
+    }
+
+    for (var i=0; i<arr2.length; i++) {
+      arr2[i] = arr2[i].join('');
+    }
+
+    while(true) {
+      var str2=str;
+      for(var i=0; i<arr2.length; i++) {
+        while(true){
+          var foundPos=str.indexOf(arr2[i]);
+          if(foundPos>=0) {
+            str=str.substring(0,foundPos) + str.substring(foundPos+2);
+          }
+          else break;
+        }
+      }
+      if(str==str2 || str.length==0) break;
+    }
+    
+    if(str.length > 0) return false;
+    else return true;
+  // }
+  // else return false; //odd length means, that at least one bracket doesn't have a pair
 }
